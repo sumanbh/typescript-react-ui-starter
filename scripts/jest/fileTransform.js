8a90c8f26c315ea0610d2a8c1c3547aba362b1d2
sumanbh <sumanbh7@gmail.com>
2022-11-09 10:29:04 -0700

    Bump deps

--- scripts/jest/fileTransform.js
+++ scripts/jest/fileTransform.js
@@ -1,20 +1,47 @@
 const path = require('path');
+const camelcase = require('camelcase');
 
 // This is a custom Jest transformer turning file imports into filenames.
-// http://facebook.github.io/jest/docs/en/webpack.html
+// https://jestjs.io/docs/webpack
+
+const VALID_CHAR_REGEX = /[^a-zA-Z0-9_-]/g;
 
 module.exports = {
-  process(src, filename) {
+  process(_, filename) {
     const assetFilename = JSON.stringify(path.basename(filename));
 
     if (filename.match(/\.svg$/)) {
-      return `module.exports = {
-        __esModule: true,
-        default: ${assetFilename},
-        ReactComponent: () => ${assetFilename},
-      };`;
+      // Based on how SVGR generates a component name:
+      // https://github.com/smooth-code/svgr/blob/01b194cf967347d43d4cbe6b434404731b87cf27/packages/core/src/state.js#L6
+      const pascalCaseFileName = camelcase(
+        path.parse(filename).name.replace(VALID_CHAR_REGEX, ''),
+        {
+          pascalCase: true,
+        }
+      );
+      const componentName = `Svg${pascalCaseFileName}`;
+      return {
+        code: `const React = require('react');
+          module.exports = {
+            __esModule: true,
+            default: ${assetFilename},
+            ReactComponent: React.forwardRef(function ${componentName}(props, ref) {
+              return {
+                $$typeof: Symbol.for('react.element'),
+                type: 'svg',
+                ref: ref,
+                key: null,
+                props: Object.assign({}, props, {
+                  children: ${assetFilename}
+                })
+              };
+            }),
+          };`,
+      };
     }
 
-    return `module.exports = ${assetFilename};`;
+    return {
+      code: `module.exports = ${assetFilename};`,
+    };
   },
 };
